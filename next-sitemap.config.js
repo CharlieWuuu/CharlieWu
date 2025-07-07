2ff8d91e523d0f0261ca9b74d14ceed19e3881ab
Charlie Wu <charliewu500@gmail.com>
2025-07-07 19:08:33 +0800

    [add] 增加 google-site-verification 與 sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://charlie-wu.vercel.app', // 改成你的網址
+    generateRobotsTxt: true, // 這樣會自動產生 robots.txt
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Noto+Serif+TC:wght@200..900&display=swap" rel="stylesheet" />
+                <meta name="google-site-verification" content="IA215bw2EghccdEy4ddIpD_a6IQixo5WPuFjQNQl5A0" />
             </head>
             <body className={styles.body}>
                 <Nav />
